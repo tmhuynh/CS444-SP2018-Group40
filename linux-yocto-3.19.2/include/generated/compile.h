52feaf0f46846ac098a16f44a6b45e6e4754ade8
Trung <huynht@oregonstate.edu>
2018-04-11 15:30:29 -0700

    Initial commit

---
+++ linux-yocto-3.19.2/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#0 SMP PREEMPT Mon Apr 9 15:08:14 PDT 2018"
+#define LINUX_COMPILE_BY "huynht"
+#define LINUX_COMPILE_HOST "os2.engr.oregonstate.edu"
+#define LINUX_COMPILER "gcc version 4.9.2 (GCC) "
