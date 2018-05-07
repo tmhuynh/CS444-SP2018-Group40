f0a43661bb721e339920633c760090c5d67fabc6
lukmand <lukmand@users.noreply.github.com>
2018-05-06 22:48:53 -0700

    Add files via upload

--- sstf-iosched.c
+++ sstf-iosched.c
@@ -1,138 +1,120 @@
-/*
- * Elevator LOOK I/O Scheduler
- * Reference: 
- * http://classes.engr.oregonstate.edu/eecs/fall2011/cs411/proj03.pdf
- */
+#include <linux/blkdev.h>
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/init.h>
+#include <linux/list.h>
+#include <linux/kernel.h>
+
 
 struct sstf_data {
 	struct list_head queue;
-
-	sector_t current_head_pos;
-	int direction;
 };
 
-static void sstf_merged_requests(struct request_queue *que, struct request *req, struct request *next) {
+static void sstf_merged_requests(struct request_queue *q, struct request *rq,
+				 struct request *next)
+{
 	list_del_init(&next->queuelist);
 }
 
-static int sstf_dispatch(struct request_queue *que, int force) {
-	struct sstf_data *ssd = que->elevator->elevator_data;
+static int sstf_dispatch(struct request_queue *q, int force)
+{
+	struct sstf_data *data = q->elevator->elevator_data;
 
-	if (!list_empty(&ssd->queue)) {
-		struct request *req, *next_req, *prev_req;
-		struct list_head *list;
+	if (!list_empty(&data->queue)) {
+		struct request *rq = list_entry(data->queue.next, struct request, queuelist);
 
-		struct current_pos = blk_rq_pos(req);
+		printk(KERN_DEBUG "SSTF: sector is dispatched at: %llu\n",blk_rq_pos(rq));
 
-		next_req = list_entry(ssd->queue.next, struct request, queuelist);
-		prev_req = list_entry(ssd->queue.prev, struct request, queuelist);
+		list_del_init(&rq->queuelist);
 
-		if (next_req == prev_req) {
-			req = next_req;
-		} else {
-			/* forward */
-			if(ssd->direction == 1) {
-				if(next_req->__sector > ssd->current_head_pos) {
-					req = next_req;
-				} else {
-					ssd->direction = 0;
-					req = prev_req;
-				}
-			} else {
-				if(prev_req->__sector < ssd->current_head_pos) {
-					req = prev_req;
-				} else {
-					ssd->direction = 1;
-					req = next_req;
-				}
-			}
-		}
-
-		list_del_init(&req->queuelist);
-
-		ssd->current_head_pos = blk_rq_pos(req) + blk_rq_sectors(req);
-
-		elv_dispatch_sort(que, req);
+		elv_dispatch_add_tail(q, rq);
+		
 		return 1;
 	}
 	return 0;
 }
 
-static void sstf_add_request(struct request_queue *que, struct request *req) {
-	struct sstf_data *ssd = que->elevator->elevator_data;
-	struct request *prev_req, *next_req;
+static void sstf_add_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *data = q->elevator->elevator_data;
+	struct list_head *curr;
+	struct list_head *temp;
+	struct request *current_node;
 
-	if(list_empty(&ssd->queue)) {
-		list_add(&req->queuelist, &ssd->queue);
-	}
+	if (list_empty(&data->queue)) {
+		printk(KERN_DEBUG "SSTF: queue empty. \n");
 
-	if(list_empty(&ssd->queue)) {
-		next_req = list_entry(ssd->queue.next, struct request, queuelist);
-		prev_req = list_entry(ssd->queue.prev, struct request, queuelist);
+		list_add(&rq->queuelist, &data->queue);
+	} else {
+		list_for_each_safe(curr, temp, &data->queue) {
+			current_node = list_entry(curr, struct request, queuelist);
 
+			if (blk_rq_pos(current_node) < blk_rq_pos(rq)) {
+				printk(KERN_DEBUG "SSTF: adding a request \n");
 
-		while (blk_rq_pos(req) > blk_rq_pos(next_req)) {
-			next_req = list_entry(next_req->queuelist.next, struct request, queuelist);
-			prev_req = list_entry(prev_req->queuelist.prev, struct request, queuelist);
+				list_add(&rq->queuelist, &current_node->queuelist);
+				break;
+			}
 		}
-
 	}
-
-	list_add_tail(&req->queuelist, &ssd->queue);
 }
 
 static struct request *
-sstf_former_request(struct request_queue *que, struct request *req) {
-	struct sstf_data *ssd = que->elevator->elevator_data;
+sstf_former_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *data = q->elevator->elevator_data;
 
-	if (req->queuelist.prev == &ssd->queue)
+	if (rq->queuelist.prev == &data->queue)
 		return NULL;
-	return list_entry(req->queuelist.prev, struct request, queuelist);
+
+	return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
 static struct request *
-sstf_latter_request(struct request_queue *que, struct request *req) {
-	struct sstf_data *ssd = que->elevator->elevator_data;
+sstf_latter_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *data = q->elevator->elevator_data;
 
-	if (req->queuelist.next == &ssd->queue)
+	if (rq->queuelist.next == &data->queue)
 		return NULL;
-	return list_entry(req->queuelist.next, struct request, queuelist);
+	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static int sstf_init_queue(struct request_queue *que, struct elevator_type *ele) {
-	struct sstf_data *ssd;
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+	struct sstf_data *data;
 	struct elevator_queue *eq;
 
-	eq = elevator_alloc(que, ele);
+	eq = elevator_alloc(q, e);
 	if (!eq)
 		return -ENOMEM;
 
-	ssd = kmalloc_node(sizeof(*ssd), GFP_KERNEL, que->node);
-	if (!ssd) {
+	data = kmalloc_node(sizeof(*data), GFP_KERNEL, q->node);
+	if (!data) {
 		kobject_put(&eq->kobj);
 		return -ENOMEM;
 	}
-	eq->elevator_data = ssd;
+	eq->elevator_data = data;
 
-	INIT_LIST_HEAD(&ssd->queue);
+	INIT_LIST_HEAD(&data->queue);
 
-	spin_lock_irq(que->queue_lock);
-	que->elevator = eq;
-	spin_unlock_irq(que->queue_lock);
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+
+	printk(KERN_DEBUG "SSTF: queue created\n");
 	return 0;
 }
 
-static void sstf_exit_queue(struct elevator_queue *ele) {
-	struct sstf_data *ssd = ele->elevator_data;
+static void sstf_exit_queue(struct elevator_queue *e)
+{
+	struct sstf_data *data = e->elevator_data;
 
-	BUG_ON(!list_empty(&ssd->queue));
-	kfree(ssd);
+	BUG_ON(!list_empty(&data->queue));
+	kfree(data);
 }
 
 static struct elevator_type elevator_sstf = {
@@ -149,18 +131,19 @@ static struct elevator_type elevator_sstf = {
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init sstf_init(void) {
+static int __init sstf_init(void)
+{
 	return elv_register(&elevator_sstf);
 }
 
-static void __exit sstf_exit(void) {
+static void __exit sstf_exit(void)
+{
 	elv_unregister(&elevator_sstf);
 }
 
 module_init(sstf_init);
 module_exit(sstf_exit);
-
-
 MODULE_AUTHOR("Amber Horvath, Davian Lukman, Trung Huynh");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("LOOK I/O scheduler");
+
